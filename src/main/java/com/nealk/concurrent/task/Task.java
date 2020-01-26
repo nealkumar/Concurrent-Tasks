44bc71f7d4201651a93ca4dda2552c3ad82fef28
Neal Kumar <nkumar20@jcu.edu>
2020-01-26 16:52:24 -0500

    Initial Commit. Added Concurrent Tasks Java Library.

---
+++ src/main/java/com/nealk/concurrent/task/Task.java
@@ -0,0 +1,18 @@
+package com.nealk.concurrent.task;
+
+import com.nealk.concurrent.ThreadSafe;
+
+public interface Task extends Runnable{
+	
+	/**
+	 * <p>Returns the thread-safe value of Type &lt?&gt</p>
+	 * 
+	 * @param <T>
+	 * @return - value of Type &lt?&gt
+	 * @throws InterruptedException
+	 * @author nealk
+	 */
+	@ThreadSafe
+	public abstract <T extends Object> T getVal() throws InterruptedException;
+
+}
